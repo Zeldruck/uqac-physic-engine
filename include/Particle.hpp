bf8779ae296f939a7ae3244515bc1d9663bfca68
vngupro <vngu.pro@gmail.com>
2023-09-17 02:13:15 +0200

    Particle class

---
+++ include/Particle.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Vector3.hpp"
+
+class Particle
+{
+	Vector3<float> position;
+	Vector3<float> velocity;
+	Vector3<float> acceleration;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string>
+#include "Vector3.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+
     cout << "hello world!" << endl;
     return 0;
 }
