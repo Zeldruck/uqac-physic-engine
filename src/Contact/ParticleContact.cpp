a3e2046ac77feed0e7279eb6db48a045fda890ac
Zeldruck <felix.jouannneau@gmail.com>
2023-10-21 18:42:45 -0400

    Contact resolver

--- src/Contact/ParticleContact.cpp
+++ src/Contact/ParticleContact.cpp
@@ -22,23 +22,29 @@ float ParticleContact::CalculateSeparatingVelocity()
 
 void ParticleContact::ResolveVelocity()
 {
-	float k = Vector3f::DotProduct((restitution + 1) * (particles->at(1)->velocity - particles->at(1)->velocity), contactNormal) /
-		(1 / particles->at(1)->mass + 1 / particles->at(1)->mass);
+	float k = Vector3f::DotProduct((restitution + 1) * (particles->at(0)->velocity - particles->at(1)->velocity), contactNormal) /
+		(1 / particles->at(0)->mass + 1 / particles->at(1)->mass);
 	
-	Vector3f v0 = particles->at(1)->velocity - k * contactNormal / particles->at(1)->mass;
+	Vector3f v0 = particles->at(0)->velocity - k * contactNormal / particles->at(0)->mass;
 	Vector3f v1 = particles->at(1)->velocity + k * contactNormal / particles->at(1)->mass;
 
 	// TODO
+
+	particles->at(0)->position += v0;
+	particles->at(1)->position += v1;
 }
 
 void ParticleContact::ResolveInterpenetration()
 {
 	if (penetration <= 0.f) return;
 
-	Vector3f Pa = (particles->at(1)->mass / particles->at(1)->mass + particles->at(1)->mass) * penetration * contactNormal;
-	Vector3f Pb = -(particles->at(1)->mass / particles->at(1)->mass + particles->at(1)->mass) * penetration * contactNormal;
+	Vector3f Pa = (particles->at(1)->mass / particles->at(0)->mass + particles->at(1)->mass) * penetration * contactNormal;
+	Vector3f Pb = -(particles->at(0)->mass / particles->at(0)->mass + particles->at(1)->mass) * penetration * contactNormal;
 
 	// TODO
+
+	particles->at(0)->position += Pa;
+	particles->at(1)->position += Pb;
 
 	// Gestion contact au repos
 }
